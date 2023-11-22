096ddf3e1784c5b1362b032bf0ad7a3e3ac4a1ad
Rachel Hamby <tnthreat33@gmail.com>
2023-11-22 11:12:30 -0500

    done

--- server/app.py
+++ server/app.py
@@ -1,6 +1,8 @@
+# server/app.py
+
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, make_response
 from flask_migrate import Migrate
 
 from models import db
@@ -13,6 +15,32 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+@app.route('/')
+def index():
+    response = make_response(
+        '<h1>Welcome to the pet/owner directory!</h1>',
+        200
+    )
+    return response
+
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id == id).first()
+
+    if not pet:
+        response_body = '<h1>404 pet not found</h1>'
+        response = make_response(response_body, 404)
+        return response
+    
+    response_body = f'''
+        <h1> Information for {pet.name}</h1>
+        <h2>Pet Species is {pet.species}</h2>
+        <h2>Pet Owner is {pet.owner.name}</h2>
+    '''
+
+    response = make_response(response_body, 200)
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
